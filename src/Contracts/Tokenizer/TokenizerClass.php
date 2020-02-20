4b8d3f52dfeacf966a5dbd24dcea648d165bb548
Antonio Codrea <antonio.codrea@evozon.com>
2020-02-20 11:34:31 +0200

    Finished tokenizing class and interface

--- src/Contracts/Tokenizer/TokenizerClass.php
+++ src/Contracts/Tokenizer/TokenizerClass.php
@@ -6,7 +6,6 @@ namespace HighlightLib\Contracts\Tokenizer;
 
 class TokenizerClass implements TokenizerInterface {
     public function tokenize(string $in): array {
-        $out = preg_split("/\s+/", $in);
-        return $out;
+        return preg_split("/ +/", $in);
     }
 }
