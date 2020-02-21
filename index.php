e9f006e8bf2c052aef45a85571d5b9bd472cda0c
Antonio Codrea <antonio.codrea@evozon.com>
2020-02-21 15:56:19 +0200

    Final

--- index.php
+++ index.php
@@ -4,4 +4,4 @@ require ('vendor/autoload.php');
 use HighlightLib\CodeHighlight;
 $path = require __DIR__ . '/Config/config.php';
 $obj = new CodeHighlight($path);
-print_r($obj->highlight('abc de efg       $abv'));
+print_r($obj->highlight('function    false    =  abc    de efg       $abv'));
