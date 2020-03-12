789811065ce73486925f15ed1c7c5f9c699c6617
Antonio Codrea <antonio.codrea@evozon.com>
2020-03-12 19:55:52 +0200

    Remove unnecessary files

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require ('vendor/autoload.php');
-
-use HighlightLib\CodeHighlight;
-$path = require __DIR__ . '/Config/config.php';
-$obj = new CodeHighlight($path);
-print_r($obj->highlight('function    false    =  abc    de efg       $abv'));
